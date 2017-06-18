4566bf8c81be891775be1921c629beaa29ea39eb
Rehan Manzoor <caprichano@gmail.com>
2017-06-18 21:15:50 +0400

    Added url support

--- index.js
+++ index.js
@@ -308,8 +308,17 @@ class Validator {
 
         alias = !!alias ? alias : name;
 
-        let message = `${alias} is not a valid string.`;
-        if (typeof value !== 'string') {
+        let message = `${alias} is not a valid url.`;
+
+        // https://stackoverflow.com/a/5717133
+        let pattern = new RegExp('^(https?:\/\/)?'+ // protocol
+            '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|'+ // domain name
+            '((\d{1,3}\.){3}\d{1,3}))'+ // OR ip (v4) address
+            '(\:\d+)?(\/[-a-z\d%_.~+]*)*'+ // port and path
+            '(\?[;&a-z\d%_.~+=-]*)?'+ // query string
+            '(\#[-a-z\d_]*)?$','i'); // fragment locater
+
+        if (!pattern.test(value)) {
             this.addValidationMessages(name, message);
         } else {
             this.removeValidationMessage(name, message);
